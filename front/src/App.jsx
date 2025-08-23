22c9dc6aa52de8e03f1eb59384dc2841180379e9
srabanimitra <srabanimitra2222@gmail.com>
2025-08-23 12:51:06 +0600

    Navbar

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/events" element={<h1>Events Page</h1>} />
+        <Route path="/featured" element={<h1>Featured Page</h1>} />
+        <Route path="/admin" element={<h1>Admin Login Page</h1>} />
+        <Route path="/login" element={<h1>Login Page</h1>} />
+        <Route path="/signup" element={<h1>Signup Page</h1>} />
+        <Route path="/contact" element={<h1>Contact Page</h1>} />
+        <Route path="/profile" element={<h1>Profile Page</h1>} />
+        <Route path="/sponsor" element={<h1>Sponsor Page</h1>} />
+        <Route path="/gallery" element={<h1>Gallery Page</h1>} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App
