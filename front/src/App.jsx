7e60786b04d96f1646d8b052924ebbbf59bb2724
srabanimitra <srabanimitra2222@gmail.com>
2025-08-23 15:16:58 +0600

    adminlogin backend

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/footer";
+import AdminLogin from "./components/AdminLogin";
+
 
 function App() {
   return (
@@ -15,17 +17,17 @@ function App() {
 
           <Route path="/events" element={<h1>Events Page</h1>} />
           <Route path="/featured" element={<h1>Featured Page</h1>} />
-          <Route path="/admin" element={<h1>Admin Login Page</h1>} />
+          <Route path="/admin" element={<AdminLogin />} />
           <Route path="/login" element={<h1>Login Page</h1>} />
           <Route path="/signup" element={<h1>Signup Page</h1>} />
           <Route path="/contact" element={<h1>Contact Page</h1>} />
           <Route path="/profile" element={<h1>Profile Page</h1>} />
           <Route path="/sponsor" element={<h1>Sponsor Page</h1>} />
           <Route path="/gallery" element={<h1>Gallery Page</h1>} />
+
           <Route path="/" element={<h1>Welcome to Eventify 🎉</h1>} />
         </Routes>
       </main>
-
       {/* Footer always at bottom */}
       <Footer />
     </Router>
